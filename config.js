2e28aae838dee4576a9c71f55a8c555f1ae4f07f
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-02-12 09:05:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -7,3 +7,18 @@ const CONFIG = {
   normalDuration: 3000,
   rareDuration: 4500
 };
+
+const CONFIG = {
+  baseRareChance: 0.1,
+  highAmountThreshold: 5000,
+  highRareChance: 0.2,
+  ultraAmountThreshold: 10000,
+  ultraRareChance: 0.3,
+  normalDuration: 3000,
+  rareDuration: 4500,
+
+  basePulse: 1.0,
+  commentInfluence: 0.0005,
+  speedBoostInfluence: 0.02,
+  overdriveMultiplier: 1.5
+};
