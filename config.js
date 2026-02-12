2bceced62ebd720b85695e82eb14fc6a35fb2907
kuzyoumike0 <ilovesakurai125_blue@yahoo.co.jp>
2026-02-12 09:45:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const CONFIG = {
   baseBPM: 72,
   minBPM: 60,
-  maxBPM: 170
+  maxBPM: 170,
+  bpmPerComment: 3,
+  amplitudeBase: 25,
+  amplitudePerSpeed: 5,
+  colorThreshold1: 90,
+  colorThreshold2: 120
 };
